28f8d8f95819b64be3806b51016d5c1e7c31982c
Zach Ehlert <zachvehlert@gmail.com>
2022-02-03 20:59:14 -0700

    Essentially finished, needs some polish

--- connect4.js
+++ connect4.js
@@ -8,35 +8,45 @@
 var WIDTH = 7;
 var HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
-function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+function makeBoard(width, height) {
+  for (var y = 0; y < height; y++) {
+    let row = [];
+    for (var x = 0; x < width; x++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  // Get "htmlBoard" variable from the item in HTML w/ID of "board"
+  const htmlBoard = document.getElementById("board");
 
-  // TODO: add comment for this code
+  // Create table row html element, set attributes and click event listener
   var top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
 
+  //Creates a headCell element WIDTH amount of times, assigns column ID, and adds the cell to the top table row
   for (var x = 0; x < WIDTH; x++) {
     var headCell = document.createElement("td");
     headCell.setAttribute("id", x);
     top.append(headCell);
   }
+  //Adds top row to htmlBoard
   htmlBoard.append(top);
 
-  // TODO: add comment for this code
+  // Creates a table row HEIGHT amount of times
+  // For each row a cell is created WIDTH amount of times, assigned xy coordinates, and appended to the row.
   for (var y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
     for (var x = 0; x < WIDTH; x++) {
@@ -44,37 +54,49 @@ function makeHtmlBoard() {
       cell.setAttribute("id", `${y}-${x}`);
       row.append(cell);
     }
+    //Each row appended to htmlBoard
     htmlBoard.append(row);
   }
 }
 
-/** findSpotForCol: given column x, return top empty y (null if filled) */
+// From the bottom of column (x), return (y) if the cell is empty. Return null if no cell in column (x) is empty.
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  for (let y = HEIGHT - 1; y > -1; y--) {
+    const cell = document.getElementById(`${y}-${x}`);
+    if (cell.firstElementChild == null) {
+      return y;
+    }
+  }
+  return null;
 }
 
-/** placeInTable: update DOM to place piece into HTML table of board */
+// Create and set attributes of div element and place it in the cell with ID of ('x-y')
 
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
+  const div = document.createElement("div");
+
+  const cell = document.getElementById(`${y}-${x}`);
+
+  div.classList.add("piece", `p${currPlayer}`);
+  cell.append(div);
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
   // TODO: pop up alert message
+  alert(`${msg}`);
 }
 
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  let x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  let y = findSpotForCol(x);
   if (y === null) {
     return;
   }
@@ -82,6 +104,8 @@ function handleClick(evt) {
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
   placeInTable(y, x);
+
+  board[y][x] = currPlayer;
 
   // check for win
   if (checkForWin()) {
@@ -93,6 +117,7 @@ function handleClick(evt) {
 
   // switch players
   // TODO: switch currPlayer 1 <-> 2
+  currPlayer = currPlayer == 1 ? 2 : 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -117,10 +142,30 @@ function checkForWin() {
 
   for (var y = 0; y < HEIGHT; y++) {
     for (var x = 0; x < WIDTH; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+      var horiz = [
+        [y, x],
+        [y, x + 1],
+        [y, x + 2],
+        [y, x + 3],
+      ];
+      var vert = [
+        [y, x],
+        [y + 1, x],
+        [y + 2, x],
+        [y + 3, x],
+      ];
+      var diagDR = [
+        [y, x],
+        [y + 1, x + 1],
+        [y + 2, x + 2],
+        [y + 3, x + 3],
+      ];
+      var diagDL = [
+        [y, x],
+        [y + 1, x - 1],
+        [y + 2, x - 2],
+        [y + 3, x - 3],
+      ];
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
@@ -129,5 +174,5 @@ function checkForWin() {
   }
 }
 
-makeBoard();
+makeBoard(WIDTH, HEIGHT);
 makeHtmlBoard();
